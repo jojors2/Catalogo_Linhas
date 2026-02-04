1f3d66e6c0c85dd97f36ec981976eb2bc111cb4b
jojors2 <joelleramos02@gmail.com>
2026-02-04 19:33:09 -0300

    att

--- Java Script/linhas.js
+++
@@ -1,8 +0,0 @@
-const linhas = [
-    {
-        id: ,
-        nome: ,
-        cod: ,
-        imagem: ,
-    }
-]
---
+++ js/linhas_amigurumi.js
@@ -0,0 +1,41 @@
+const linhas_amigurumi = [
+    // --- NEUTROS E PELE ---
+    { id: 1, nome: "Branco", cod: "8001", imagem: "assets/cores/8001-branco.png" },
+    { id: 2, nome: "Off-White", cod: "8176", imagem: "assets/cores/8176-offwhite.png" },
+    { id: 3, nome: "Natural", cod: "20", imagem: "assets/cores/20-natural.png" },
+    { id: 4, nome: "Gardênia", cod: "3061", imagem: "assets/cores/3061-gardenia.png" },
+    { id: 5, nome: "Porcelana", cod: "7564", imagem: "assets/cores/7564-porcelana.png" },
+    { id: 6, nome: "Chantilly", cod: "7563", imagem: "assets/cores/7563-chantilly.png" },
+    { id: 7, nome: "Amêndoa", cod: "7650", imagem: "assets/cores/7650-amendoa.png" },
+    { id: 8, nome: "Castanha", cod: "7625", imagem: "assets/cores/7625-castanha.png" },
+    { id: 9, nome: "Pedra Quente", cod: "7066", imagem: "assets/cores/7066-pedraquente.png" },
+    { id: 10, nome: "Castor", cod: "7603", imagem: "assets/cores/7603-castor.png" },
+    { id: 11, nome: "Lenha", cod: "7713", imagem: "assets/cores/7713-lenha.png" },
+    { id: 12, nome: "Cacau", cod: "7567", imagem: "assets/cores/7567-cacau.png" },
+
+    // --- ROSAS E VERMELHOS ---
+    { id: 13, nome: "Quartzo", cod: "3077", imagem: "assets/cores/3077-quartzo.png" },
+    { id: 14, nome: "Bailarina", cod: "6085", imagem: "assets/cores/6085-bailarina.png" },
+    { id: 15, nome: "Rosa Choque", cod: "6161", imagem: "assets/cores/6161-rosachoque.png" },
+    { id: 16, nome: "Paixão", cod: "3611", imagem: "assets/cores/3611-paixao.png" },
+    { id: 17, nome: "Carmim", cod: "3528", imagem: "assets/cores/3528-carmim.png" },
+
+    // --- AZUIS E VERDES ---
+    { id: 18, nome: "Acqua", cod: "2532", imagem: "assets/cores/2532-acqua.png" },
+    { id: 19, nome: "Candy", cod: "2012", imagem: "assets/cores/2012-candy.png" },
+    { id: 20, nome: "Hortênsia", cod: "2137", imagem: "assets/cores/2137-hortensia.png" },
+    { id: 21, nome: "Bic", cod: "2829", imagem: "assets/cores/2829-bic.png" },
+    { id: 22, nome: "Menta", cod: "5743", imagem: "assets/cores/5743-menta.png" },
+    { id: 23, nome: "Pistache", cod: "5800", imagem: "assets/cores/5800-pistache.png" },
+    { id: 24, nome: "Verde Bandeira", cod: "5398", imagem: "assets/cores/5398-verdebandeira.png" },
+
+    // --- AMARELOS E LARANJAS ---
+    { id: 25, nome: "Canário", cod: "1289", imagem: "assets/cores/1289-canario.png" },
+    { id: 26, nome: "Solar", cod: "1317", imagem: "assets/cores/1317-solar.png" },
+    { id: 27, nome: "Cenoura", cod: "4448", imagem: "assets/cores/4448-cenoura.png" },
+
+    // --- ESCUROS E CINZAS ---
+    { id: 28, nome: "Prata", cod: "8473", imagem: "assets/cores/8473-prata.png" },
+    { id: 29, nome: "Chumbo", cod: "8990", imagem: "assets/cores/8990-chumbo.png" },
+    { id: 30, nome: "Preto", cod: "8990", imagem: "assets/cores/8990-preto.png" }
+];
